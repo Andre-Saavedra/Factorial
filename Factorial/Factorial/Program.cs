d536150a59b6cb0349234bb26e94cc5378b61f88
andre saavedra <andre.saavedra@hotmail.com>
2020-12-29 23:32:26 +0000

    Factorial

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace Factorial
+namespace Fatorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculo do fatorial");
+
+            Console.WriteLine("Introduza um número");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+
+            if (n < 0)
+            {
+                Console.WriteLine("O número deve ser positivo");
+                return;
+            }
+
+            double factorial = 1;
+
+            int i = 2;
+            while (i <= n)
+
+            {
+                factorial *= i;
+                i++;
+            }
+
+            Console.WriteLine($"{n}! = {factorial:0.#}");
         }
     }
 }
